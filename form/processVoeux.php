f1479402d4e122bf50bdc566e4906174319bed4f
FlorianLouargant <florian.louargant@iut-rodez.fr>
2018-04-11 23:40:41 +0200

    [ADD] CSS pages destination, liste destinations et liste voeux
    ajout du CSS
    Modification des image pour transparence
    "CSS flo" à copier dans "style.css"

--- form/processVoeux.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Florian
- * Date: 30/01/2018
- * Time: 11:42
- */
--- js/voeuxArray.js
+++ js/voeuxArray.js
@@ -9,23 +9,25 @@ function afficherGroupe() {
 }
 
 $(window).on("load",function(){
+    
     var fixHelperModified = function(e, tr) {
-            var $originals = tr.children();
-            var $helper = tr.clone();
-            $helper.children().each(function(index) {
-                $(this).width($originals.eq(index).width())
-            });
-            return $helper;
-        },
+    var $originals = tr.children();
+    var $helper = tr.clone();
+    $helper.children().each(function(index) {
+        $(this).width($originals.eq(index).width())
+    });
+    return $helper;
+    },
         updateIndex = function(e, ui) {
-            $('td.index', ui.item.parent()).each(function (i) {
+            $('td.tdIndex', ui.item.parent()).each(function (i) {
                 $(this).html(i + 1);
             });
         };
 
-
     $("#sort tbody").sortable({
+       /* items: "> tr:not(#rowSeparation)",*/
         helper: fixHelperModified,
         stop: updateIndex
     }).disableSelection();
+    
 });
